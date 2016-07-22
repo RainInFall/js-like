47b858fad4684bd59edd6ec94e3356311003e728
RainInFall <onlycursedname@gmail.com>
2016-07-22 09:07:37 +0800

    move test files to another dir

--- test/array_ptr_test.go
+++ test/array_ptr_test.go
@@ -8,22 +8,6 @@ import (
 
 //go:generate $GOPATH/bin/js-like array *Int
 
-type Int struct {
-	Value int
-}
-
-func (array ArraypInt) Len() int {
-	return len(array)
-}
-
-func (array ArraypInt) Less(i, j int) bool {
-	return false
-}
-
-func (array ArraypInt) Swap(i, j int) {
-	array[i], array[j] = array[j], array[i]
-}
-
 func TestComplie(t *testing.T) {
 	assert.Init(t)
 
---
+++ test/def.go
@@ -0,0 +1,17 @@
+package main
+
+type Int struct {
+	Value int
+}
+
+func (array ArraypInt) Len() int {
+	return len(array)
+}
+
+func (array ArraypInt) Less(i, j int) bool {
+	return false
+}
+
+func (array ArraypInt) Swap(i, j int) {
+	array[i], array[j] = array[j], array[i]
+}
